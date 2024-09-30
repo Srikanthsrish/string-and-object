cf170d2c7a238f37665d133e5dbeafc16d2b4147
Srikanthsrish <srishbontha83@gmail.com>
2024-09-30 15:58:08 +0530

    first commit

---
+++ task1.js
@@ -0,0 +1,15 @@
+let string1 = "Hello";
+let string2 = "World";
+let result = "";
+
+
+for (let i = 0; i < string1.length; i++) {
+    result += string1[i];
+}
+
+
+for (let j = 0; j < string2.length; j++) {
+    result += string2[j];
+}
+
+console.log("Result:", result); 
